91053faa5760a8f63590caa0a313a63fbd3576c3
yuwenzhenggong <165892315+yuwenzhenggong@users.noreply.github.com>
2024-10-21 11:39:56 +0800

    Add files via upload

---
+++ Solution7.java
@@ -0,0 +1,120 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+/**
+ * @description:
+ *
+ * 给你一个字符串 s，以及该字符串中的一些「索引对」数组 pairs，其中 pairs[i] = [a, b] 表示字符串中的两个索引（编号从 0 开始）。
+ *
+ * 你可以 任意多次交换 在 pairs 中任意一对索引处的字符。
+ *
+ * 返回在经过若干次交换后，s 可以变成的按字典序最小的字符串。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入：s = "dcab", pairs = [[0,3],[1,2]]
+ * 输出："bacd"
+ * 解释：
+ * 交换 s[0] 和 s[3], s = "bcad"
+ * 交换 s[1] 和 s[2], s = "bacd"
+ * 示例 2：
+ *
+ * 输入：s = "dcab", pairs = [[0,3],[1,2],[0,2]]
+ * 输出："abcd"
+ * 解释：
+ * 交换 s[0] 和 s[3], s = "bcad"
+ * 交换 s[0] 和 s[2], s = "acbd"
+ * 交换 s[1] 和 s[2], s = "abcd"
+ * 示例 3：
+ *
+ * 输入：s = "cba", pairs = [[0,1],[1,2]]
+ * 输出："abc"
+ * 解释：
+ * 交换 s[0] 和 s[1], s = "bca"
+ * 交换 s[1] 和 s[2], s = "bac"
+ * 交换 s[0] 和 s[1], s = "abc"
+ *
+ *
+ * 提示：
+ *
+ * 1 <= s.length <= 10^5
+ * 0 <= pairs.length <= 10^5
+ * 0 <= pairs[i][0], pairs[i][1] < s.length
+ * s 中只含有小写英文字母
+ *
+ */
+
+public class Solution7 {
+
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        if (pairs.size() <= 1) {
+            return s;
+        }
+
+        // 将任意交换的结点对输入并查集
+        int len = s.length();
+        UnionFind unionFind = new UnionFind(len);
+        for (List<Integer> pair : pairs) {
+            int index1 = pair.get(0);
+            int index2 = pair.get(1);
+            unionFind.union(index1, index2);
+        }
+
+        // 构建映射关系
+        char[] charArray = s.toCharArray();
+        // key：连通分量的代表元，value：同一个连通分量的字符集合（保存在一个优先队列中）
+        Map<Integer, PriorityQueue<Character>> hashMap = new HashMap<>(len);
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            hashMap.computeIfAbsent(root, key -> new PriorityQueue<>()).offer(charArray[i]);
+        }
+
+        // 重组字符串
+        char[] result = new char[len];
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            result[i] = hashMap.get(root).poll();
+        }
+        return new String(result);
+    }
+
+    private class UnionFind {
+        private final int[] parent;
+        private final int[] rank;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.parent[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+                rank[rootX] += rank[rootY];
+            } else {
+                parent[rootX] = rootY;
+                rank[rootY] += rank[rootX];
+            }
+        }
+
+        public int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+    }
+}
